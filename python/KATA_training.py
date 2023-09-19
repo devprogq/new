17fec63eb12425a84f994d7d87df8fb4391f1627
Lautaro <devprog001@gmail.com>
2023-09-19 12:10:34 -0300

    Subida a git.

---
+++ python/KATA_training.py
@@ -0,0 +1,41 @@
+# KATAS TRAINING
+
+# 1st https://www.codewars.com/kata/53ee5429ba190077850011d4 (resolved with ChatGPT)
+
+def double_integer(n):
+    return n*2
+
+# 2nd https://www.codewars.com/kata/544675c6f971f7399a000e79 (resolved with ChatGPT)
+
+def string_to_number(string):
+
+    return int(string)
+
+# 3rd https://www.codewars.com/kata/583710ccaa6717322c000105 (resolved with docs)
+
+def simple_multiplication(n) :
+    
+    if n % 2 == 0:
+        number = n*8
+    else:
+        number = n*9
+
+    return number
+
+# 4th https://www.codewars.com/kata/5a00e05cc374cb34d100000d (resolved with ChatGPT)
+
+def reverse_seq(n):
+    
+    return list(range(n, 0, -1))
+
+# 5th https://www.codewars.com/kata/53dc54212259ed3d4f00071c (resolved with docs)
+
+def sum_array(arr):
+    
+    sum = 0
+
+    for i in arr:
+        sum = sum + i
+
+    return sum
+
